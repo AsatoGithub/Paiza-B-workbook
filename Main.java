10979d2aa5b4e83e2dd834fc21c3ecc2a79b49ce
AsatoT <20195046@g.aut.ac.jp>
2021-08-06 23:58:08 +0900

    PaizaB問題集（Java）

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] Array = str.split(" ");
+        for(int i = 0;i<Array.length;i++){
+            System.out.println(Array[i]);
+        }
+    }
+}
