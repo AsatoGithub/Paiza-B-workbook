655823e277fc228a40cbac78624d9250f4411714
AsatoT <20195046@g.aut.ac.jp>
2021-08-08 23:08:14 +0900

    commit

--- Main.java
+++ Main.java
@@ -4,7 +4,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
-        System.out.println(line.toUpperCase());
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int i = a;i<=b;i++){
+          System.out.println(i);
+        }
+
     }
 }
