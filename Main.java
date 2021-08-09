27d0863bcdf6d39fcc7b97938b5935f753e84ff4
AsatoT <20195046@g.aut.ac.jp>
2021-08-09 23:43:54 +0900

    commit

--- Main.java
+++ Main.java
@@ -4,10 +4,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        sc.nextLine();
         String line = sc.nextLine();
-        System.out.println(line.substring(0,a - 1) + line.substring(a - 1, b).toUpperCase() + line.substring(b));
-        }
+        System.out.println(line.length());
     }
+}
