177773c50abfe0ceb190cb73a459d2dba9e8b061
AsatoT <20195046@g.aut.ac.jp>
2021-08-09 23:54:20 +0900

    commit

--- Main.java
+++ Main.java
@@ -4,9 +4,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] line = {"Nara", "Shiga", "Hokkaido", "Chiba"};
-        for(int i = 0;i<line.length;i++){
-           System.out.println(line[i]);
+        String line = sc.nextLine();
+        String[] Array = line.split("");
+        for(int i = 0; i<Array.length;i++){
+        System.out.println(Array[i]);
         }
     }
 }
