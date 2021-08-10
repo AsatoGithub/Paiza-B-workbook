ca72337b07022f963c75f8deb9ab58cc29718d1c
AsatoT <20195046@g.aut.ac.jp>
2021-08-10 13:17:33 +0900

    commit

--- Main.java
+++ Main.java
@@ -4,7 +4,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
         String[] line = sc.nextLine().split("");
-        System.out.println(line[0] + " "+ line[1]);
+        System.out.println(line[n-1] + " " + line[n]);
     }
 }
