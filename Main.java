6e59fb8db7c47e77e92d7bf766927fc47396bf86
AsatoT <20195046@g.aut.ac.jp>
2021-08-10 16:14:39 +0900

    commit

--- Main.java
+++ Main.java
@@ -4,18 +4,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] line = {"HND", "NRT", "KIX", "NGO", "NGO"};
+        String[] line = {"HND", "NRT", "KIX", "NGO", "NGO", "NGO", "NGO", "NGO"};
         int count = 0;
+        int counter = 0;
         for(int i = 0;i<line.length-1;i++){
             if(line[i].equals(line[i+1])){
                 count = count + 1;
             }
+            counter = counter + 1;
         }
-        if(0<count){
-            System.out.println("true");
-
-        }else{
-            System.out.println("false");
+        if(line[counter].equals(line[counter-1])){
+            count = count + 1;
         }
+            System.out.println(count);
     }
 }
