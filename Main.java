4e7c052dd2032ca0338b2721fa2079385dc4bbc0
AsatoT <20195046@g.aut.ac.jp>
2021-08-10 13:48:28 +0900

    comitt

--- Main.java
+++ Main.java
@@ -4,9 +4,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        String[] line = sc.nextLine().split("");
-        System.out.println(line[n-1] + " " + line[n]);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+        int count = 0;
+        for(int i = 0;i<=t.length() - s.length();i++){
+            String string = t.substring(i, i + s.length());
+            if(string.equals(s)){
+                count = count + 1;
+            }
+        }
+        System.out.println(count);
     }
 }
