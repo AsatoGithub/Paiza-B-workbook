1e1928c1cde7dfa5a3a28d0898b7a5edeb243c3c
AsatoT <20195046@g.aut.ac.jp>
2021-08-17 00:40:58 +0900

    commit

--- Main.java
+++ Main.java
@@ -1,21 +1,30 @@
 import java.util.*;
-
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] n = {1, 3, 5, 6, 3, 2, 5, 23, 2};
-        for(int i = 0; i<n.length-1; i++){
-            for(int m = i + 1;m<n.length;m++){
-                if(n[i]>n[m]){
-                    int a = n[i];
-                    n[i] = n[m];
-                    n[m] = a;
+        HashMap<Integer, String> HS = new HashMap<Integer, String>();
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] S = new String[n];
+        int[] D = new int[n];
+        for(int i = 0; i<n; i++){
+            S[i] = sc.next();
+            D[i] = sc.nextInt();
+            HS.put(D[i],S[i]);
+        }
+        for(int l = 0; l<D.length-1; l++){
+            for(int m = l + 1; m<D.length; m++){
+                if(D[l]>D[m]){
+                    int a = D[l];
+                    D[l] = D[m];
+                    D[m] = a;
                 }
             }
         }
-        for(int i = 0;i<n.length;i++){
-            System.out.println(n[i]);
+        for(int i = 0; i<D.length; i++){
+            System.out.println(HS.get(D[i]));
         }
 
     }
