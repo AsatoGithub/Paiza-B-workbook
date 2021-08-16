89cedbb30e5278f4b6885dbcacdb40c842e28ea3
AsatoT <20195046@g.aut.ac.jp>
2021-08-16 23:31:27 +0900

    commit

--- Main.java
+++ Main.java
@@ -4,22 +4,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] Array = new int[n];
-        for(int i = 0;i<n;i++){
-            Array[i] = sc.nextInt();
-        }
-        for (int m = 0; m < Array.length - 1; m++) {
-            for (int j = m + 1; j < Array.length ; j++) {
-                if (Array[m] > Array[j]) {
-                    int a = Array[m];
-                    Array[m] = Array[j];
-                    Array[j] = a;
+        int[] n = {1, 3, 5, 6, 3, 2, 5, 23, 2};
+        for(int i = 0; i<n.length-1; i++){
+            for(int m = i + 1;m<n.length;m++){
+                if(n[i]>n[m]){
+                    int a = n[i];
+                    n[i] = n[m];
+                    n[m] = a;
                 }
             }
         }
-        for (int i = 0; i < Array.length; i++) {
-            System.out.println(Array[i]);
+        for(int i = 0;i<n.length;i++){
+            System.out.println(n[i]);
         }
+
     }
 }
