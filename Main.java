76f1fcbae314edf8b6ef21fafa47fd701912f9c2
AsatoT <20195046@g.aut.ac.jp>
2021-08-17 23:52:11 +0900

    commit

--- Main.java
+++ Main.java
@@ -6,12 +6,24 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.nextLine();
-        String[] S = new String[n];
-        int[] D = new int[n];
+        String[] A = new String[n];
+        int[] a = new int[n];
         for(int i = 0; i<n; i++){
-            S[i] = sc.next();
-            D[i] = sc.nextInt();
-            System.out.println(D[i]);
+            A[i] = sc.next();
+            a[i] = Integer.parseInt(A[i]);
         }
+        for(int m = 0; m<a.length; m++){
+            for(int j = m + 1; j<a.length; j++){
+                if(a[m]>a[j]){
+                    int k = a[m];
+                    a[m] = a[j];
+                    a[j] = k;
+                }
+            }
+        }
+        for(int i = 0; i<a.length; i++){
+            System.out.println(a[i]);
+        }
+
     }
 }
