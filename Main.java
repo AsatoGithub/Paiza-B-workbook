e3954c519df1a3b6462789a1c665c8dcad4b242a
AsatoT <20195046@g.aut.ac.jp>
2021-08-17 22:03:48 +0900

    commit

--- Main.java
+++ Main.java
@@ -1,31 +1,25 @@
 import java.util.*;
-import java.util.HashMap;
+
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        HashMap<Integer, String> HS = new HashMap<Integer, String>();
+        int answer = 0;
         int n = sc.nextInt();
         sc.nextLine();
-        String[] S = new String[n];
-        int[] D = new int[n];
+        int[] a = new int[n];
+        int[] b = new int[n];
         for(int i = 0; i<n; i++){
-            S[i] = sc.next();
-            D[i] = sc.nextInt();
-            HS.put(D[i],S[i]);
+            a[i] = Integer.parseInt(sc.next());
+            b[i] = sc.nextInt();
         }
-        for(int l = 0; l<D.length-1; l++){
-            for(int m = l + 1; m<D.length; m++){
-                if(D[l]>D[m]){
-                    int a = D[l];
-                    D[l] = D[m];
-                    D[m] = a;
-                }
+        for(int m = 0; m<n; m++){
+            if(a[m] == b[m]){
+                answer = answer + (a[m] * b[m]);
+            }else{
+                answer = answer + a[m] + b[m];
             }
         }
-        for(int i = 0; i<D.length; i++){
-            System.out.println(HS.get(D[i]));
-        }
-
+        System.out.println(answer);
     }
 }
