ebe5043c228f117bab35397772e536730362faba
AsatoT <20195046@g.aut.ac.jp>
2021-08-17 22:37:02 +0900

    commit

--- Main.java
+++ Main.java
@@ -4,15 +4,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        String c = sc.nextLine();
-        int count = 0;
-        for(int i = 0; i <= c.length() - s.length(); i++) {
-            String a = c.substring(i, i + s.length());
-            if (a.equals(s)) {
-                count = count + 1;
-            }
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] S = new String[n];
+        int[] D = new int[n];
+        for(int i = 0; i<n; i++){
+            S[i] = sc.next();
+            D[i] = sc.nextInt();
+            System.out.println(D[i]);
         }
-        System.out.println(count);
     }
 }
