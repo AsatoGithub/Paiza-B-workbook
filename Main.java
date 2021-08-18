53cb0073c89281b2454e21517b75d6a0e4eff668
AsatoT <20195046@g.aut.ac.jp>
2021-08-18 15:11:41 +0900

    commit

--- Main.java
+++ Main.java
@@ -1,31 +1,11 @@
 import java.util.*;
 
+
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    sc.nextLine();
-    HashMap<String, Integer> map = new HashMap<>();
-
-    for (int i=0; i<n; i++) {
-      String c = sc.next();
-      int v = sc.nextInt();
-      if (map.containsKey(c))
-        map.put(c, map.get(c) + v);
-      else
-        map.put(c, v);
-    }
-
-    List<Integer> ll = new ArrayList<>(map.values());
-    Collections.sort(ll);
-    Collections.reverse(ll);
-
-    for (int v : ll){
-      for (String c : map.keySet()){
-        if (map.get(c) == v){
-          System.out.println(c + " " + v);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i<3; i++){
+        System.out.println(sc.nextLine());
         }
-      }
     }
-  }
 }
