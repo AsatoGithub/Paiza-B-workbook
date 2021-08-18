d4a56e026105c321cdf81a59c265463f324c486c
AsatoT <20195046@g.aut.ac.jp>
2021-08-18 15:09:27 +0900

    commit

--- Main.java
+++ Main.java
@@ -1,29 +1,31 @@
 import java.util.*;
 
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        String[] A = new String[n];
-        int[] a = new int[n];
-        for(int i = 0; i<n; i++){
-            A[i] = sc.next();
-            a[i] = Integer.parseInt(A[i]);
-        }
-        for(int m = 0; m<a.length; m++){
-            for(int j = m + 1; j<a.length; j++){
-                if(a[m]>a[j]){
-                    int k = a[m];
-                    a[m] = a[j];
-                    a[j] = k;
-                }
-            }
-        }
-        for(int i = 0; i<a.length; i++){
-            System.out.println(a[i]);
-        }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.nextLine();
+    HashMap<String, Integer> map = new HashMap<>();
 
+    for (int i=0; i<n; i++) {
+      String c = sc.next();
+      int v = sc.nextInt();
+      if (map.containsKey(c))
+        map.put(c, map.get(c) + v);
+      else
+        map.put(c, v);
     }
+
+    List<Integer> ll = new ArrayList<>(map.values());
+    Collections.sort(ll);
+    Collections.reverse(ll);
+
+    for (int v : ll){
+      for (String c : map.keySet()){
+        if (map.get(c) == v){
+          System.out.println(c + " " + v);
+        }
+      }
+    }
+  }
 }
