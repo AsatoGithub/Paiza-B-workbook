9d55368869502598b378692aad523d16d2e03327
AsatoT <20195046@g.aut.ac.jp>
2021-08-19 13:07:50 +0900

    commit

--- Main.java
+++ Main.java
@@ -3,11 +3,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] line = new String[5];
-        for(int i = 0; i<5; i++){
-                line[i] = sc.nextLine();
-            System.out.println(line[i]);
+        Scanner sc = new Scanner(System.in);;
+        String s = sc.nextLine();
+        if (s.equals("OOOOO")) {
+            System.out.println("O");
+        }else if(s.equals("XXXXX")) {
+            System.out.println("X");
+        } else {
+            System.out.println("D");
         }
     }
 }
