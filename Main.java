4eb003618a9112e0cb4d3b3aa105cad4b1020857
AsatoT <20195046@g.aut.ac.jp>
2021-08-19 13:18:10 +0900

    commit

--- Main.java
+++ Main.java
@@ -3,14 +3,22 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);;
-        String s = sc.nextLine();
-        if (s.equals("OOOOO")) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        String s3 = sc.nextLine();
+        String s4 = sc.nextLine();
+        String s5 = sc.nextLine();
+        String O = "OOOOO";
+        String X = "XXXXX";
+
+        if(s1.equals(O) || s2.equals(O) || s3.equals(O) || s4.equals(O) || s5.equals(O)){
             System.out.println("O");
-        }else if(s.equals("XXXXX")) {
+        }else if(s1.equals(X) || s2.equals(X) || s3.equals(X) || s4.equals(X) || s5.equals(X)){
             System.out.println("X");
-        } else {
+        }else{
             System.out.println("D");
         }
+
     }
 }
