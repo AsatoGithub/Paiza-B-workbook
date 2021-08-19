8b2b41370f69063687ced691c8eaf88ad7d0f488
AsatoT <20195046@g.aut.ac.jp>
2021-08-19 14:08:10 +0900

    commit

--- Main.java
+++ Main.java
@@ -10,11 +10,11 @@ public class Main {
             String string = sc.nextLine();
             Array.add(string);
         }
-        String[] S = new String[Array.size()];
-        for(int i = 0; i<S.length; i++){
-            S[i] = String.valueOf(Array.get(0).charAt(i))+String.valueOf(Array.get(1).charAt(i)+String.valueOf(Array.get(2).charAt(i))+String.valueOf(Array.get(3).charAt(i))+String.valueOf(Array.get(4).charAt(i)));
+        String[] S = new String[2];
+            S[0] = String.valueOf(Array.get(0).charAt(0))+String.valueOf(Array.get(1).charAt(1)+String.valueOf(Array.get(2).charAt(2))+String.valueOf(Array.get(3).charAt(3))+String.valueOf(Array.get(4).charAt(4)));
+            S[1] = String.valueOf(Array.get(0).charAt(4))+String.valueOf(Array.get(1).charAt(3)+String.valueOf(Array.get(2).charAt(2))+String.valueOf(Array.get(3).charAt(1))+String.valueOf(Array.get(4).charAt(0)));
 
-        }
+
         for(int n = 0; n<S.length; n++){
             if(S[n].equals("OOOOO")){
                 System.out.println("O");
