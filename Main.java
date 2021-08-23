c317c6beeddd76d4b81fd62633281a66f6ff6aaa
AsatoT <20195046@g.aut.ac.jp>
2021-08-23 22:11:40 +0900

    commit

--- Main.java
+++ Main.java
@@ -4,12 +4,21 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String T = sc.nextLine();
-        int m = sc.nextInt();
-        LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
-        for(int i = 0; i<m; i++){
-            map.put(sc.next(),sc.next());
+        LinkedHashMap<String, String> Usermap = new LinkedHashMap<String, String>();
+        LinkedHashMap<String, String> Bloodmap = new LinkedHashMap<String, String>();
+
+        String U = sc.nextLine();
+        int n = sc.nextInt();
+        for(int i = 0; i<n; i++){
+            Usermap.put(sc.next(),sc.next());
         }
-        System.out.println(map.get(T));
+
+        int m = sc.nextInt();
+        for(int i = 0; i<m; i++){
+            Bloodmap.put(sc.next(),sc.next());
+        }
+
+        String BloodType = Usermap.get(U);
+        System.out.println(Bloodmap.get(BloodType));
     }
 }
