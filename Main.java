c0558c0be916a0857def742a7f6e7155b30ed625
AsatoT <20195046@g.aut.ac.jp>
2021-08-23 22:03:46 +0900

    commit

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
 import java.util.*;
-import java.util.HashMap;
+
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    LinkedHashMap<String, String> hs = new LinkedHashMap<String, String>();
-    String s = sc.nextLine();
-    int n = sc.nextInt();
-    for(int i = 0; i<n; i++){
-        hs.put(sc.next(),sc.next());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String T = sc.nextLine();
+        int m = sc.nextInt();
+        LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+        for(int i = 0; i<m; i++){
+            map.put(sc.next(),sc.next());
+        }
+        System.out.println(map.get(T));
     }
-      System.out.println(s + " " + hs.get(s));
-  }
 }
