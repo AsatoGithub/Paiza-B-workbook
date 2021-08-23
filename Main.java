4b20287a1ce99ff0237456981bb34b78bbd79162
AsatoT <20195046@g.aut.ac.jp>
2021-08-23 22:24:50 +0900

    commit

--- Main.java
+++ Main.java
@@ -4,22 +4,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        LinkedHashMap<String, String> Usermap = new LinkedHashMap<String, String>();
-        LinkedHashMap<String, String> Bloodmap = new LinkedHashMap<String, String>();
-
-        int n = sc.nextInt();
-        for(int i = 0; i<n; i++){
-            Usermap.put(sc.next(),sc.next());
-        }
-
-        int m = sc.nextInt();
-        for(int i = 0; i<m; i++){
-            Bloodmap.put(sc.next(),sc.next());
-        }
-
-        for(String Key: Usermap.keySet()){
-            System.out.println(Key + " " + Bloodmap.get(Usermap.get(Key)));
-        }
-
+        String line = sc.nextLine();
+        System.out.println(line.length());
     }
 }
